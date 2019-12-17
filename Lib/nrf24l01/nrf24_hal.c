bcf900909b3773c093ffb5f34c32ff0d5feaf1a1
kongr45gpen <electrovesta@gmail.com>
2019-12-17 14:43:36 +0200

    Add NRF24 library by @dimst23

---
+++ Lib/nrf24l01/nrf24_hal.c
@@ -0,0 +1,4 @@
+//
+// Created by kongr45gpen on 16/12/19.
+//
+
---
+++ Lib/nrf24l01/nrf24_hal.h
@@ -0,0 +1,44 @@
+#ifndef __NRF24_HAL_H
+#define __NRF24_HAL_H
+
+
+// Hardware abstraction layer for NRF24L01+ transceiver (hardware depended functions)
+// GPIO pins definition
+// GPIO pins initialization and control functions
+// SPI transmit functions
+
+
+// Peripheral libraries
+#include "stm32l4xx.h"
+//#include <stm32f10x_spi.h>
+
+
+// SPI port peripheral
+//#define nRF24_SPI_PORT             SPI2
+
+// nRF24 GPIO peripherals
+#define nRF24_GPIO_PERIPHERALS     (RCC_APB2ENR_IOPAEN)
+
+// CE (chip enable) pin (PB11)
+#define nRF24_CE_PORT              GPIOA
+#define nRF24_CE_PIN               GPIO_PIN_3
+#define nRF24_CE_L()               HAL_GPIO_WritePin(nRF24_CE_PORT, nRF24_CE_PIN, GPIO_PIN_RESET)
+#define nRF24_CE_H()               HAL_GPIO_WritePin(nRF24_CE_PORT, nRF24_CE_PIN, GPIO_PIN_SET)
+
+// CSN (chip select negative) pin (PB12)
+#define nRF24_CSN_PORT             GPIOA
+#define nRF24_CSN_PIN              GPIO_PIN_4
+#define nRF24_CSN_L()              HAL_GPIO_WritePin(nRF24_CSN_PORT, nRF24_CSN_PIN, GPIO_PIN_RESET)
+#define nRF24_CSN_H()              HAL_GPIO_WritePin(nRF24_CSN_PORT, nRF24_CSN_PIN, GPIO_PIN_SET)
+
+
+// IRQ pin (PB10)
+#define nRF24_IRQ_PORT             GPIOA
+#define nRF24_IRQ_PIN              GPIO_PIN_2
+
+
+// Function prototypes
+void nRF24_GPIO_Init(void);
+uint8_t nRF24_LL_RW(uint8_t data);
+
+#endif // __NRF24_HAL_H
